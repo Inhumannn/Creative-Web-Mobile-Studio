4f4f73b61bc2f86bd68ec191e32b52d643157a2f
Inhumannn <thomas.pnbm@gmail.com>
2025-03-31 16:52:09 +0200

    feat: add UI components and config files
    - Added `components.json` for UI config.
    - Added `jsconfig.json` for path aliases.
    - Created `src/components/ui` directory.
    - Created `src/lib/utils` for helper functions.
    - Implemented Accordion component.

--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -1,10 +1,86 @@
+import { MessageCircle } from "lucide-react";
+import Footer from "../components/footer";
+import Header from "../components/header";
+
 function Blog() {
   return (
-    <sfd>
-      dsf
-      <p>sdf</p>
-      ok
-    </sfd>
+    <>
+      <Header />
+      <main>
+        <h1 className="text-[33px] text-center text-[#333] pt-20 pb-5">
+          Lastest Blogs
+        </h1>
+        <p className="text-[28px] text-center">
+          The creative web & mobile studio and focused on brand identity, web
+          development and social marketing.
+        </p>
+        <section className="py-10">
+          <img
+            src="/img/blog-image1.jpg"
+            alt="image libre de droit"
+            className="w-full h-auto"
+          />
+          <h2 className="text-[28px] py-3">
+            We Help You Create Perfect Modern Design
+          </h2>
+          <article className="flex items-center gap-3.5 pb-7">
+            <img
+              src="/img/author-image1.jpg"
+              alt="image libre de droit"
+              className="w-[12%] rounded-[100%]"
+            />
+            <p>Jen Lopez</p>
+            <p>Jan 30, 2016</p>
+            <div className="flex">
+              <MessageCircle />
+              <p>124Comments</p>
+            </div>
+          </article>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi culpa
+            sit enim fugiat consequuntur assumenda. Impedit, corrupti deserunt
+            debitis quis, officia rem ex quod consectetur mollitia beatae ut,
+            dolore nobis!
+          </p>
+          <button className="p-7 border-[3px] rounded-[10%]">
+            Continue Reading
+          </button>
+        </section>
+        <section className="py-10">
+          <img
+            src="/img/blog-image2.jpg"
+            alt="image libre de droit"
+            className="w-full h-auto"
+          />
+          <h2 className="text-[28px] py-3">
+            When do you marry with someone you love in life?
+          </h2>
+          <article className="flex items-center gap-3.5 pb-7">
+            <img
+              src="/img/author-image2.jpg"
+              alt="image libre de droit"
+              className="w-[12%] rounded-[100%]"
+            />
+            <p>Leo Dennis</p>
+            <p>November 22, 2016</p>
+            <div className="flex">
+              <MessageCircle />
+              <p>23 Comments</p>
+            </div>
+          </article>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi culpa
+            sit enim fugiat consequuntur assumenda. Impedit, corrupti deserunt
+            debitis quis, officia rem ex quod consectetur mollitia beatae ut,
+            dolore nobis!
+          </p>
+          <button className="p-7 border-[3px] rounded-[10%]">
+            Continue Reading
+          </button>
+        </section>
+      </main>
+      <Footer />
+    </>
   );
 }
 export default Blog;
--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,4 +1,20 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@radix-ui/react-accordion";
+
 function Contact() {
-  return <></>;
+  return (
+    <Accordion type="single" collapsible>
+      <AccordionItem value="item-1">
+        <AccordionTrigger>Page en cours de création</AccordionTrigger>
+        <AccordionContent>
+          Merci de patienter, cette page sera crée sous peu...
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
+  );
 }
 export default Contact;
