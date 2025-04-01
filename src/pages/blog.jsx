a64e02815ccbc7e48cc0a561c0936853074d93b5
Inhumannn <thomas.pnbm@gmail.com>
2025-04-01 10:16:29 +0200

    feat: improve blog and contact pages
    - Updated styling for better responsiveness
    - Improved text sizes and layouts
    - Added new contact form
    - Removed old tsconfig files
    - Added new components for UI elements

--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -6,11 +6,11 @@ function Blog() {
   return (
     <>
       <Header />
-      <main>
-        <h1 className="text-[33px] text-center text-[#333] pt-20 pb-5">
+      <main className="max-w-[1120px] m-auto">
+        <h1 className="text-[27px] text-center text-[#333] pt-20 pb-5 sm:text-[37px]">
           Lastest Blogs
         </h1>
-        <p className="text-[28px] text-center">
+        <p className="text-[18px] text-center sm:text-[30px]">
           The creative web & mobile studio and focused on brand identity, web
           development and social marketing.
         </p>
@@ -20,10 +20,10 @@ function Blog() {
             alt="image libre de droit"
             className="w-full h-auto"
           />
-          <h2 className="text-[28px] py-3">
+          <h2 className="text-[18px] py-3 sm:text-[23px]">
             We Help You Create Perfect Modern Design
           </h2>
-          <article className="flex items-center gap-3.5 pb-7">
+          <article className="flex items-center gap-3.5 pb-7 text-[12px] sm:text-[15px]">
             <img
               src="/img/author-image1.jpg"
               alt="image libre de droit"
@@ -32,7 +32,7 @@ function Blog() {
             <p>Jen Lopez</p>
             <p>Jan 30, 2016</p>
             <div className="flex">
-              <MessageCircle />
+              <MessageCircle className="size-[16px] sm:size-4.5 self-center" />
               <p>124Comments</p>
             </div>
           </article>
@@ -42,7 +42,7 @@ function Blog() {
             debitis quis, officia rem ex quod consectetur mollitia beatae ut,
             dolore nobis!
           </p>
-          <button className="p-7 border-[3px] rounded-[10%]">
+          <button className="my-7 p-2.5 border-[3px] rounded-[10%]">
             Continue Reading
           </button>
         </section>
@@ -52,10 +52,10 @@ function Blog() {
             alt="image libre de droit"
             className="w-full h-auto"
           />
-          <h2 className="text-[28px] py-3">
+          <h2 className="text-[19px] py-3 sm:text-[23px]">
             When do you marry with someone you love in life?
           </h2>
-          <article className="flex items-center gap-3.5 pb-7">
+          <article className="flex flex-wrap items-center gap-3.5 pb-7 text-[12px] sm:text-[15px]">
             <img
               src="/img/author-image2.jpg"
               alt="image libre de droit"
@@ -64,7 +64,7 @@ function Blog() {
             <p>Leo Dennis</p>
             <p>November 22, 2016</p>
             <div className="flex">
-              <MessageCircle />
+              <MessageCircle className="size-[16px] sm:size-4.5 self-center" />
               <p>23 Comments</p>
             </div>
           </article>
@@ -74,7 +74,7 @@ function Blog() {
             debitis quis, officia rem ex quod consectetur mollitia beatae ut,
             dolore nobis!
           </p>
-          <button className="p-7 border-[3px] rounded-[10%]">
+          <button className="my-7 p-2.5 border-[3px] rounded-[10%]">
             Continue Reading
           </button>
         </section>
--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,20 +1,56 @@
+import Footer from "@/components/footer";
 import {
   Accordion,
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
-} from "@radix-ui/react-accordion";
+} from "@/components/ui/accordion";
+import Header from "../components/header";
 
 function Contact() {
   return (
-    <Accordion type="single" collapsible>
-      <AccordionItem value="item-1">
-        <AccordionTrigger>Page en cours de création</AccordionTrigger>
-        <AccordionContent>
-          Merci de patienter, cette page sera crée sous peu...
-        </AccordionContent>
-      </AccordionItem>
-    </Accordion>
+    <>
+      <Accordion type="single" collapsible>
+        <AccordionItem value="item-1">
+          <AccordionTrigger>Menu</AccordionTrigger>
+          <AccordionContent>
+            <Header />
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+
+      <main className="max-w-[1120px] m-auto">
+        <h1 className="text-[27px] text-center text-[#333] pt-20 pb-5 sm:text-[37px]">
+          get in touch
+        </h1>
+        <p className="text-[19px] text-center sm:text-[30px]">
+          The creative web & mobile studio and focudsed on brand identity, web
+          development and social marketing.
+        </p>
+        <form action="" className="grid grid-cols-1 gap-12 py-18">
+          <input type="text" name="name" id="name" placeholder="Name" />
+          <input type="email" name="mail" id="mail" placeholder="Email" />
+          <input type="text" name="tel" id="tel" placeholder="Phone Number" />
+          <select name="" id="">
+            <option value="Lorem ipsum dolor sit amet.">
+              Lorem ipsum dolor sit amet.
+            </option>
+            <option value="Lorem ipsum dolor sit amet.">
+              Lorem ipsum dolor sit amet.
+            </option>
+            <option value="Lorem ipsum dolor sit amet.">
+              Lorem ipsum dolor sit amet.
+            </option>
+            <option value="Lorem ipsum dolor sit amet.">
+              Lorem ipsum dolor sit amet.
+            </option>
+          </select>
+          <textarea name="" id="" placeholder="Project Details"></textarea>
+          <button>Send Message</button>
+        </form>
+      </main>
+      <Footer />
+    </>
   );
 }
 export default Contact;
